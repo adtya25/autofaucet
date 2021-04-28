6d4354e1e2db44aa00e571d06b5dae70b5746723
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-04-28 08:14:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+//Jangan Lupa Subrek Ngap
+//Gw Jadi Ngeri anjing
+
+$UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4490.0 Safari/537.36";
+
+
+//Cookie BTT
+$BTT = "BTTToken=sBKMeoWWfgVOnkvzxin72TZeLU03LtYs";
+
+//Cookie TRX
+$TRX = "TRXToken=V0aPt1hwgV2L3k4B0lfkVODEmknDN4Xx";
+
+//Cookie DOGE
+$DOGE = "DOGEToken=ul0uotr3SVUPk4lYqNsoSs4BpHJZD3il";
+
+//Cookie LTC
+$LTC = "LTCToken=gATly9aISNvjbSHucXgOtZKlbh36NWPM";
+
+
